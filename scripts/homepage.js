e5874f98255d923ac6834ffda5bb4e01b0fd1f4e
lisbet_dokcom <lissbetjb13@gmail.com>
2025-03-12 06:05:10 +0100

    commit

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,9 +4,9 @@ document.getElementById("currentyear").innerHTML = currentYear;
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 
-function getCelsius(fahrenheit) {
-	return (fahrenheit - 32) * (5/9);
-}
+// function getCelsius(fahrenheit) {
+// 	return (fahrenheit - 32) * (5/9);
+// }
 
-document.querySelector("#temp").value = getCelsius(33).toFixed(1);
+// document.querySelector("#temp").value = getCelsius(33).toFixed(1);
 
